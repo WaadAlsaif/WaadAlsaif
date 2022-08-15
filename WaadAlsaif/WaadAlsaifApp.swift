4b238574f1cf92abc514cb429146ad7fbad20300
WAAD ALSAIF <50026099+WaadAlsaif@users.noreply.github.com>
2022-08-16 02:54:35 +0300

    Initial Commit

---
+++ WaadAlsaif/WaadAlsaifApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaadAlsaifApp.swift
+//  WaadAlsaif
+//
+//  Created by Waad Alsaif on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WaadAlsaifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
